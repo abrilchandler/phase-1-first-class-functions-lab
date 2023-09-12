26b2090394cc7ebfafcdbb6b7cc81b650009bf59
Chandler Abril <abrilchandler1@gmail.com>
2023-09-12 05:55:02 -0700

    Finally

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = () => {
+    return ["Antonia", "Nuru"] 
+}
+
+const returnLastTwoDrivers = () => {
+    return["Amari", "Mo"]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (multi) => {
+    return (fare) => {
+       return fare * multi
+    }
+}
+
+const fareDoubler = (createFareMultiplier) => {
+    return createFareMultiplier * 2;
+}
+
+const fareTripler = (createFareMultiplier) => {
+    return createFareMultiplier * 3
+}
+
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers("Antonia", "Nuru");
+}
